e95b3743253b72b7a7c375f4ea8b8e0c8d24762f
Evan Bolyen <evan@cym.is>
2025-02-06 13:15:34 -0700

    support readthedocs

--- q2doc/drivers/common.py
+++ q2doc/drivers/common.py
@@ -2,7 +2,13 @@ import os
 import urllib
 
 def _build_url(data_dir, fn):
-    baseurl = os.environ.get('BASE_URL', '/')
+    baseurl = os.environ.get('BASE_URL')
+    if baseurl is None:
+        baseurl = os.environ.get('READTHEDOCS_CANONICAL_URL')
+    if baseurl is None:
+        # prevent myst from treating it as a local ref (and clobbering the url)
+        baseurl = '/'
+
     parts = list(urllib.parse.urlparse(baseurl))
     parts[2] += '/'.join([str(data_dir), str(fn)])
     url = urllib.parse.urlunparse(parts)
